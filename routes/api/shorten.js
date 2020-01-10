6553f385ea9cc5b3bda29993fda8850db393931a
Andrea Mastrantoni <anmastrantoni@gmail.com>
2020-01-09 22:10:09 -0500

    Fixed funtionality of shotening urls

--- routes/api/shorten.js
+++ routes/api/shorten.js
@@ -1,7 +1,39 @@
 //Require
 const router = require("express").Router();
-const uniqid = require("uniqid");
+const shortid = require("shortid");
 
+//Picking charcter set for shortid generation
+shortid.characters(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_"
+);
+
+// Load URL Model
+const URL = require("../../models/Urls");
+
+// @route GET /api/shorten/test
+// @desc Test API end point
+// @access public
 router.get("/test", (req, res) => res.json({ msg: "API is working" }));
+
+// @route POST /api/shorten
+// @desc Post a URL to shorten
+// @access public
+router.post("/", (req, res) => {
+  let urlData;
+  if (req.body.url) {
+    urlData = req.body.url;
+  }
+  console.log("URL is : ", urlData);
+
+  const shortlyLink = new URL({
+    url: urlData,
+    hash: shortid.generate()
+  });
+
+  shortlyLink
+    .save()
+    .then(() => res.json("URL Added!"))
+    .catch(err => res.status(400).json("Error: " + err));
+});
 
 module.exports = router;
